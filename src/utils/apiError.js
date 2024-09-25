0967848602ea902c2403ea8ebd915ce0a52236e1
ShakilOfficials <mrshakilhossainofficial@gmail.com>
2024-09-25 15:16:39 +0600

    update utils for api

---
+++ src/utils/apiError.js
@@ -0,0 +1,20 @@
+class apiError extends Error {
+  constructor(
+    statuscode,
+    message = "Something Went Wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statuscode = statuscode;
+    (this.data = null),
+      (this.message = message = false),
+      (this.success = false),
+      (this.errors = this.errors);
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
