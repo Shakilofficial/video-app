5d02302a6e27d03a93a3d050c371a022f3049a88
ShakilOfficials <mrshakilhossainofficial@gmail.com>
2024-09-29 23:28:18 +0600

    update package.json

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // require('dotenv').config({ path: './env' });
 import dotenv from 'dotenv';
-import connectDB from "./db/index.js";
 import { app } from './app.js';
+import connectDB from "./db/index.js";
 dotenv.config({
-  path: "./.env",
-});
+  path: './.env'
+})
 
 connectDB()
     .then(() => {
