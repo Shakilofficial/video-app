2b2aa12407a05f71f7d91f5bc5926ecf22f9c75d
ShakilOfficials <mrshakilhossainofficial@gmail.com>
2024-09-30 00:27:10 +0600

    update db and index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 // require('dotenv').config({ path: './env' });
-import dotenv from 'dotenv';
-import { app } from './app.js';
+import dotenv from "dotenv";
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
+
 dotenv.config({
   path: "./.env",
 });
 
 connectDB()
-    .then(() => {
-        app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️  Server is running at Port: ${process.env.PORT}`);
-    })
-})
-    .catch(err => {
-    console.log("MongoDB connection Failed !!! err");
-})
-
-
-
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`🌐 Server is running at Port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed !!! ", err);
+  });
 
 /* async () => {
   try {
